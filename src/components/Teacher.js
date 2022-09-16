e9009ef5caab79d7acae8ca1c90bbc14e5f8a0d2
Lek-San <leksan.webdev@gmail.com>
2022-09-16 12:08:35 +0200

    update teacher.js

--- src/components/Teacher.js
+++ src/components/Teacher.js
@@ -1,123 +1,112 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTable } from 'react-table';
+import Table from './Table';
+import format from 'date-fns/format';
 import { Button } from '@mantine/core';
-import { IconPower } from '@tabler/icons';
-import { auth } from '../firebase';
 
-const Header = () => {
+
+const Teacher = () => {
+
+	const [data, setData] = useState();
+	const [grades, setGrades] = useState();
 	const [resits, setResits] = useState();
 
-	let navigate = useNavigate();
-
-	const data = React.useMemo(() => resits || [], [resits]);
+	
+	
 
 	useEffect(() => {
-		fetch('http://localhost:9000/resits')
-			.then((res) => res.json())
-			.then((data) => setResits(data))
-			.catch((err) => console.log('catched fetch error :', err));
+		fetch("http://localhost:9000/resits")
+		.then(response => response.json())
+		.then(response => {
+			setData(response);
+			//console.log(format(response[0].resitDate,'dd-MM-yyyy'));
+		}
+			)
+		.catch(error => console.log(error))
+
+		
+
 	}, []);
 
-	const columns = React.useMemo(
-		() => [
-			{
-				Header: 'Rattrapage',
-				accessor: 'name', // accessor is the "key" in the data
-			},
-			{
-				Header: 'Sujet',
-				accessor: 'exam',
-			},
-			{
-				Header: 'Date',
-				accessor: 'resitDate',
-			},
-			{
-				Header: 'Status',
-				accessor: 'status',
-			},
-		],
-		[]
-	);
+	useEffect(() => {
+		fetch("http://localhost:9000/grades")
+		.then(responseOnHttp => responseOnHttp.json())
+		.then(data => setGrades(data))
+		.catch(error => console.log("catched fetch error :", error))
 
-	console.log(resits);
 
-	const tableInstance = useTable({ columns, data });
 
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+		//Creation d'un nouveau objet qui s'appele "element" pour nouveau element
+		if(data) {
+			setResits(data.map((element) => {return {
+				//(1=resit=le mouveau objet) (2=l'objet resit) (3=id de l'encient objet) 
+				id : element.id, 
+				name : element.name,
+				exam : element.exam,
+				date : format(element.resitDate, 'dd-MM-yyyy'),
+				teacherName : element.teacher.lastName,
+				oversserName : element.overseer.name,
+				status : element.status,
+			}}))
+			
+		}
 
-	const filterByTeacherId = (id) => {
-		fetch(`http://localhost:9000/resitsbyteacher?id=${id}`)
-			.then((res) => res.json())
-			.then((data) => setResits(data))
-			.catch((err) => console.log('catched fetch error :', err));
+		
+
+	}, [data])
+
+	const resitsHeaders = [
+		{
+			Header: 'Id',
+			accessor: 'id', // accessor is the "key" in the data
+		},
+		{
+			Header: 'Sujet',
+			accessor: 'name',
+		},
+		{
+			Header: 'Exam',
+			accessor: 'exam',
+		},
+		{
+			Header: 'Date',
+			accessor: 'date',
+		},
+		{
+			Header: 'Professeur',
+			accessor: 'teacherName',
+		},
+		{
+			Header: "Surveillant",
+			accessor: 'oversserName',
+		},
+		{
+			Header: "Status",
+			accessor: 'status',
+		},
+	];
+
+	const onSingleEdit = (row) => {
+		console.log("dfdg")
 	};
-
-	const reset = (id) => {
-		fetch(`http://localhost:9000/resits`)
-			.then((res) => res.json())
-			.then((data) => setResits(data))
-			.catch((err) => console.log('catched fetch error :', err));
+	const onSingleDelete = (row) => {
+		console.log('delited !', row.id);
 	};
+	
 
 	return (
-		<div className='p-4'>
-			<h2 className='text-center text-2xl font-bold m-8'>Bienvenue Mathieu</h2>
+		<div className='content-center'>
 
-			<Button onClick={reset} className='inline-flex font-bold mt-8 mb-8 bg-blue-300 mr-4 px-6 py-2 text-white rounded shadow hover:bg-blue-500 transition duration-300'>
-				Tous
-			</Button>
+			<div className='p-4'>
 
-			{resits &&
-				resits.map((resit) => (
-					<Button onClick={() => filterByTeacherId(resit.teacher.id)} className='inline-flex font-bold mt-8 mb-8 bg-blue-300 mr-4 px-6 py-2 text-white rounded shadow hover:bg-blue-500 transition duration-300'>
-						{resit.teacher.email}
-					</Button>
-				))}
-			<>
-				<table {...getTableProps()}>
-					<thead>
-						{headerGroups.map((headerGroup) => (
-							<tr {...headerGroup.getHeaderGroupProps()}>
-								{headerGroup.headers.map((column) => (
-									<th {...column.getHeaderProps()}>{column.render('Header')}</th>
-								))}
-							</tr>
-						))}
-					</thead>
-					<tbody {...getTableBodyProps()}>
-						{resits ? (
-							rows.map((row) => {
-								prepareRow(row);
-								return (
-									<tr {...row.getRowProps()}>
-										{row.cells.map((cell) => {
-											return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-										})}
-									</tr>
-								);
-							})
-						) : (
-							<tr>
-								<td>...</td>
-							</tr>
-						)}
-					</tbody>
-				</table>
-			</>
+				<h2 className='text-center text-2xl font-bold m-8'>Bienvenue</h2>
 
-			{console.log(resits)}
-
-			<IconPower
-				color='green'
-				className='absolute right-4 top-4'
-				onClick={() => {
-					auth.signOut();
-					navigate(`/`);
-				}}></IconPower>
+				{ 
+					resits && <Table dataTable={resits} headers={resitsHeaders} onSingleEdit={onSingleEdit} onSingleDelete={onSingleDelete}/>
+				}
+				
+			</div>
 		</div>
 	);
 };
 
-export default Header;
+export default Teacher;
