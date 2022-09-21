47354f211f30165b68c8d70359125679b997e8fe
moicdjam <moicdjam@gmail.com>
2022-09-21 17:18:29 +0200

    formated date + conditional layout

--- src/components/Overseer.js
+++ src/components/Overseer.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import format from 'date-fns/format';
 import Table from './Table';
-
-
+import { IconArrowBack } from '@tabler/icons';
 
 const Overseer = () => {
 
 	const [resits, setResits] = useState();
 	const [grades, setGrades] = useState();
+	const [selectedResit, setSelectedResit] = useState();
+	const [step, setStep] = useState('resitsList');
 	
-	
-
-	useEffect(() => {
+		useEffect(() => {
 		fetch('http://localhost:9000/resits/noef')
 			.then((res) => res.json())
 			.then((res) => {
@@ -21,33 +20,31 @@ const Overseer = () => {
 
 	}, []);
 
-	
-
-	const gradeResit= (row) => {
+	const studentList= (row) => {
 		fetch(`http://localhost:9000/gradesbyresit?id=${row.id}`)
 			.then((res) => res.json())
 			.then((data) => setGrades(data))
 			.catch((err) => console.log('catched fetch error :', err));
+
+			setStep('studentList');
+			setSelectedResit(row);
 	
 	};
 
-	const onSingleEdit= (row) => {
-		console.log("first")
-	
-	};
 
-	
 
 	const resitsHeaders = [
 		{
 			Header: 'Rattrapage',
 			accessor: 'name',
-		},
-		
+		},		
 		{
 			Header: 'Date',
-			accessor: 'resitDate',
-		},
+			Cell: ({ cell: { row: { original: { resitDate } } } }) => {
+				return format(resitDate, 'dd-MM-yyyy')
+			}
+
+		},	
 		{
 			Header: 'Status',
 			accessor: 'status',
@@ -70,27 +67,39 @@ const Overseer = () => {
 		
 	]
 
-	
-	
 	const onSingleDelete = (row) => {
 		console.log('delited !', row.id);
 	};
 
 	return (
-		<>
-			
+		<div className='p-4'>
+		{resits && step === 'resitsList' && (
+			<>
+				<h2 className='text-center text-2xl font-bold m-8'>Liste des Rattrapages</h2>
+				<Table dataTable={resits} headers={resitsHeaders} onSingleEdit={studentList} onSingleDelete={onSingleDelete} />
+			</>
+		)}
 
-		{
-	resits && <Table dataTable={resits} headers={resitsHeaders} onSingleEdit={gradeResit} onSingleDelete={onSingleDelete} />
-		}
+		 {resits && console.log(resits)}
 
-		{
-	grades && <Table dataTable={grades} headers={gradresitHeaders} onSingleEdit={onSingleEdit} onSingleDelete={onSingleDelete} />
-
-}
-			
-		</>
-	)
+		{grades && step === 'studentList' && (
+			<>
+				<h2 className='text-center text-2xl font-bold mt-8'>Liste des Étudiants</h2>
+				<p className='text-center'>
+					Pour le module <span className='font-medium italic uppercase'>{selectedResit.name}</span> du {selectedResit.date}
+				</p>
+				<IconArrowBack
+					className='absolute top-28 cursor-pointer hover:text-teal-500 transition duration-200 hover:shadow-teal-500 hover:scale-[.9]'
+					size={38}
+					onClick={() => {
+						setStep('resitsList');
+					}}
+				/>
+				<Table dataTable={grades} headers={gradresitHeaders} onSingleEdit={() => console.log("first")} onSingleDelete={onSingleDelete} />
+			</>
+		)}
+	</div>
+);
 };
 
 export default Overseer;
