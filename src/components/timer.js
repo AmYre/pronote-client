b155d51c5336d0ca343f4011beee66ad34b65827
moicdjam <moicdjam@gmail.com>
2022-10-04 16:57:12 +0200

    btn accessible 15 mn avant epreuve

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,87 +1,93 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { Button } from '@mantine/core';
 
-const Timer = () => {
-  
-const Ref = useRef(null);
-  
-// The state for our timer
-const [timer, setTimer] = useState('00:00:00');
+const Timer = (date) => {
 
+    const Ref = useRef(null);
 
+    // The state for our timer
+    const [timer, setTimer] = useState('00:00:00');
+    const [showButton, setshowButton] = useState(false);
+    const [showTimer, setshowTimer] = useState(false);
 
-const startTimer = (e ,id) => {
+    const startTimer = (e, id) => {
 
-                
-     if (e.getSeconds() < 60)
-     e.setSeconds(e.getSeconds() +1)
+        if (e.getSeconds() < 60)
+            e.setSeconds(e.getSeconds() + 1)
 
-     if (e.getSeconds() > 59)
-     e.setMinutes(e.getMinutes()+1 )
+        if (e.getSeconds() > 59)
+            e.setMinutes(e.getMinutes() + 1)
 
-     if (e.getMinutes() > 59)
-     e.setHours(e.getHours() +1)
+        if (e.getMinutes() > 59)
+            e.setHours(e.getHours() + 1)
 
-     if (e.getHours() == 1)
-     id.clearInterval()
+        if (e.getHours() == 1)
+            id.clearInterval()
 
-     setTimer(e.getHours() + ":" + e.getMinutes() + ":" + e.getSeconds())
+        setTimer(e.getHours() + ":" + e.getMinutes() + ":" + e.getSeconds())
 
-        // update the timer
-        // check if less than 10 then we need to 
-        // add '0' at the beginning of the variable
-      
+    }
+
+    const clearTimer = (e) => {
+
+        setTimer('00:00:00');
+
+        if (Ref.current) clearInterval(Ref.current);
+        const id = setInterval(() => {
+            startTimer(e, id);
+        }, 1000)
+        Ref.current = id;
+    }
+
+    const getDeadTime = () => {
+        let deadline = new Date();
+
+        deadline.setSeconds(0);
+        deadline.setMinutes(0);
+        deadline.setHours(0);
+        return deadline;
+    }
+
+    // We can use useEffect so that when the component
+    // mount the timer will start as soon as possible
+
+    // We put empty array to act as componentDid
+    // mount only
+    useEffect(() => {
+        clearTimer(getDeadTime());
+        function getDifferenceInSeconds(date1, date2) {
+            const diffInMs = Math.abs(date2 - date1);
+            return diffInMs / 1000;
+
+        }
+        setInterval(() => {
+            const today = new Date(Date.now())
+            const dif = getDifferenceInSeconds(date.date, today)
+            console.log(dif);
+            if (Math.floor(dif) == 0)
+                setshowbutton(true);
+        }, 1000);
+    }, []);
+
+    // Another way to call the clearTimer() to start
+    // the countdown is via action event from the
+    // button first we create function to be called
+    // by the button
+    const onClickReset = () => {
+        clearTimer(getDeadTime());
+    }
+
+    return (
+        <div className="App">
+
+            {showButton &&
+                <>
+
+                    <Button onClick={onClickReset}>Reset</Button>
+                </>
+            }
+        </div>
+    )
 }
+export default Timer
 
-const clearTimer = (e) => {
-
-    // If you adjust it you should also need to
-    // adjust the Endtime formula we are about
-    // to code next    
-    setTimer('00:00:00');
-
-    // If you try to remove this line the 
-    // updating of timer Variable will be
-    // after 1000ms or 1sec
-    if (Ref.current) clearInterval(Ref.current);
-    const id = setInterval(() => {
-        startTimer(e,id);
-    }, 1000)
-    Ref.current = id;
-}
-
-const getDeadTime = () => {
-    let deadline = new Date();
-
-    // This is where you need to adjust if 
-    // you entend to add more time
-    deadline.setSeconds(0);
-    deadline.setMinutes(0);
-    deadline.setHours(0);
-    return deadline;
-}
-
-// We can use useEffect so that when the component
-// mount the timer will start as soon as possible
-
-// We put empty array to act as componentDid
-// mount only
-useEffect(() => {
-    clearTimer(getDeadTime());
-}, []);
-
-// Another way to call the clearTimer() to start
-// the countdown is via action event from the
-// button first we create function to be called
-// by the button
-const onClickReset = () => {
-    clearTimer(getDeadTime());
-}
-
-return (
-    <div className="App">
-        <h2>{timer}</h2>
-        <button onClick={onClickReset}>Reset</button>
-    </div>
-)
-}
- export default Timer
